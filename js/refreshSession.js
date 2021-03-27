b6693b7f843a4bad356ddc304e085cf7931968e6
Konstantinos Konstantopoulos <37417801+ntinouldinho@users.noreply.github.com>
2021-03-27 20:00:59 +0200

    Delete refreshSession.js

--- js/refreshSession.js
+++
@@ -1,8 +0,0 @@
-	
-	setInterval( function(){
-		var refreshAPI = 'https://fragoulakis.gr/platform/refreshSession.php';
-		$.post(refreshAPI, function(data, status){
-			console.log(data);
-		});
-
-	}, 900000) //every 15minutes
